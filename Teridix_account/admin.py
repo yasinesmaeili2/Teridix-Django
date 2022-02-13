ae217a50068e1b7a34fb84d8807f4fa95f4add75
Yasin Esmaeili <testwebsite1244@gmail.com>
2022-02-13 10:40:08 +0330

    Fixed

--- Teridix_account/admin.py
+++ Teridix_account/admin.py
@@ -6,18 +6,6 @@ from django.contrib.auth.admin import UserAdmin
 
 # add field UserModel for showing
 
-
-# UserAdmin.fieldsets[4][1] += (
-#     'is_active',
-#     'is_staff',
-#     'is_superuser',
-#     'is_auther',
-#     'special_user'
-#     'groups',
-#     'user_permissions',
-# )
-
-
 UserAdmin.fieldsets += (
     (None,{'fields':('is_auther',)}),
 )
