fd827d9c7fadab3fb328cfebe2b21421cbfece84
Yasin Esmaeili <testwebsite1244@gmail.com>
2022-02-13 13:14:58 +0330

    change get_abolute_url for detail view and change jinja

--- Teridix_main/views.py
+++ Teridix_main/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
-
+from .models import (
+    Blog,
+    Category
+)
 
 
 def BlogView(request):
-    return render(request,'Views/blog.html')
+    category = Category.objects.all()
+
+    c = {
+        'category':category
+    }
+
+    return render(request,'Views/blog.html',c)
 
 
 def BlogSingleView(requeset,slug,pk):
+    blog = Blog.objects.filter(status='T').order_by('create')
 
-    return render(requeset,'Views/blog-single.html')
+    c = {
+        'posts':blog
+    }
+
+    return render(requeset,'Views/blog-single.html',c)
 
 
         
