b6ebcf8252c7f5b6707bdc3514e69a8ce365e2f0
Yasin Esmaeili <testwebsite1244@gmail.com>
2022-02-14 16:57:35 +0330

    add account view for manager posts and other

--- Teridix_account/urls.py
+++ Teridix_account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from .views import (
+    AccountView
+)
 
 
 app_name = 'Teridix_account'
 
 urlpatterns = [
-    
+    path('account',AccountView,name='account')   
 ]
--- Teridix_account/views.py
+++ Teridix_account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from Teridix_main.models import Blog
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+@login_required
+def AccountView(request):
+
+    blog = Blog.objects.all()
+    c = {
+        'posts':blog
+    }
+    return render(request,'View/accounts.html',c)
