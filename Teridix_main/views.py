920002dab417b54739f2bd9a2d23066daf7958f3
Yasin Esmaeili <testwebsite1244@gmail.com>
2022-02-14 09:22:23 +0330

    change last post in blog view

--- Teridix_main/views.py
+++ Teridix_main/views.py
@@ -8,14 +8,18 @@ from .models import (
 
 def BlogView(request):
     blog = Blog.objects.filter(status='T')
+    blog_order = Blog.objects.filter(status='T').order_by('-create')
     category = Category.objects.all()
+    
+    #ls count post by category
     count_post_by_category = Category.objects.all().annotate(blogs_count=Count('blog'))
 
 
     c = {
         'category':category,
         'posts':blog,
-        'cc':count_post_by_category
+        'cc':count_post_by_category,
+        'bOrder':blog_order
     }
 
     return render(request,'Views/blog.html',c)
@@ -25,6 +29,8 @@ def BlogSingleView(requeset,slug,pk):
     
     blog = get_object_or_404(Blog,slug=slug,id=pk)
     blog_order = Blog.objects.filter(status='T').order_by('-create')
+    
+    # count post by category
     count_post_by_category = Category.objects.all().annotate(blogs_count=Count('blog'))
 
     c = {
