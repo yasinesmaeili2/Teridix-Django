724241c9d2f61c5fa6ac02bca744abf2a10b0487
Yasin Esmaeili <testwebsite1244@gmail.com>
2022-02-16 14:06:13 +0330

    Add form register for authentication

--- Teridix_account/forms.py
+++ Teridix_account/forms.py
@@ -1,4 +1,64 @@
+from re import L
 from django import forms
+from django.core import validators
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class SingupForm(forms.Form):
+    UserName = forms.CharField(widget=forms.TextInput(
+        attrs={'class':'form-control','placeholder':'نام کاربری'}
+        ),validators=[
+            validators.MaxLengthValidator(limit_value=20,message='نام کاربری نمیتواند بیشتر از 20 کاراکتر باشد'),
+            validators.MinLengthValidator(limit_value=8,message='نام کاربری نمیتواند کمتر از 8 کاراکتر باشد')
+        ]
+        )
+
+    Email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class':'form-control','placeholder':'hello@example.com'}
+        ),
+        validators=[
+            validators.EmailValidator(message='ایمیل وارد شده صحیح نمیباشد')
+        ]
+        )
+
+    Password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class':'form-control','placeholder':'رمز'}
+        ))
+
+    Confirm_Password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class':'form-control','placeholder':'رمز دوباره'}
+        ))
+
+
+    def clean_UserName(self):
+        Username = self.cleaned_data.get('UserName')
+        filtering = User.objects.filter(username=Username)
+        if filtering.exists():
+            raise forms.ValidationError('این نام کاربری وجود دارد')
+        return Username
+
+    def clean_Email(self):
+        Email = self.cleaned_data.get('Email')
+        filtering = User.objects.filter(email=Email)
+        if filtering.exists():
+            raise forms.ValidationError('با این ایمیل قبلا ثبت نام شده است')
+        return Email
+
+    def clean_Confirm_Password(self):
+        data = self.cleaned_data
+        p1 = self.cleaned_data.get('Password')
+        p2 = self.cleaned_data.get('Confirm_Password')
+
+        if p1 and p2:
+            if p1 != p2:
+                raise forms.ValidationError('رمز عبور فرق میکند')
+            if len(p1) < 8:
+                raise forms.ValidationError('رمز عبور نباید کمتر از 8 کاراکتر باشد')
+        return data
+
+
 
 
 
