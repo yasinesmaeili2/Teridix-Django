ea82e7da37a7b5087ac3f34d958ca7a17df87dc0
Yasin Esmaeili <testwebsite1244@gmail.com>
2022-02-22 18:38:57 +0330

    add password reset view for send email and add file reset-password-email.html

--- Teridix/urls.py
+++ Teridix/urls.py
@@ -13,15 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth.views import PasswordResetView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Teridix_main.urls')),
-    path('',include('Teridix_account.urls'))
+    path('',include('Teridix_account.urls')),
+
+    # 1
+    path('password-reset/',PasswordResetView.as_view(),name='PRE')
+    
+    
 ]
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
