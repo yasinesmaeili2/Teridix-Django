f6c0348f2ac2e257dac4d516240103e4643faba3
Yasin Esmaeili <testwebsite1244@gmail.com>
2022-02-22 19:44:22 +0330

    Add new field email in User model for unique email

--- Teridix_account/models.py
+++ Teridix_account/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     is_auther = models.BooleanField(default=False,verbose_name='وضعیت نویسندگی')
