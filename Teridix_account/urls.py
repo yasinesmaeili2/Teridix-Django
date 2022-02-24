68a09f921dc3ab1f84206e113567aa1115e5a188
Yasin Esmaeili <testwebsite1244@gmail.com>
2022-02-24 16:42:04 +0330

    Fixed

--- Teridix_account/urls.py
+++ Teridix_account/urls.py
@@ -16,7 +16,6 @@ app_name = 'Teridix_account'
 urlpatterns = [
     path('account',AccountView,name='account'),
     path('account/sign-in/',LoginView,name='login'),
-    # path('account/sign-up/',RegisterView,name='register'),
     path('account/sing-out/',LogoutRequest,name='logout'),
     path('account/update-view/<int:pk>/',Updating.as_view(),name='update'),
     path('account/create-view/',Creating.as_view(),name='create'),
